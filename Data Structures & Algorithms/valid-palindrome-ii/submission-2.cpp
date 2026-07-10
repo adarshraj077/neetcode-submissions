e2718208ea86556cba12d4c630b75dff46239afd
adarsh raj <spratima46154@gmail.com>
2026-07-10 11:28:59 +0530

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+   bool check(string s,int left,int right){
+    
+      while(left<right){
+        if(s[left++] != s[right--]){
+            return false;
+        }
+      }
+      return true;
+   }
+
+    bool validPalindrome(string s) {
+         string alnustring="";
+        //getting only alnum
+        for(char x:s){
+          if(isalnum(x)){
+            alnustring+=tolower(x);
+          }
+        }
+        // 
+        int left=0,right=alnustring.size()-1;
+
+        while(left<right){
+            if(alnustring[left] != alnustring[right]){
+                return check(alnustring,left+1,right) || check(alnustring,left,right-1);
+            }
+            left++;
+            right--;
+        }
+
+     return true;
+
+    
+    }
+};
